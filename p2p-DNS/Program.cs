015207f320540cf8d359ef923bf63b9687e7af53
unknown <Martin@.(none)>
2010-12-02 21:06:05 +0000

    Initial empty project

---
+++ p2p-DNS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2p_DNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
